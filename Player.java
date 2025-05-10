75fd081e83d57c39928281499b6c18583a167ce4
Michael Lee <michaelee1081@gmail.com>
2025-05-09 20:36:51 -0700

    Wrote some method headers for Player 5/9/2025 8:36 PM

--- Player.java
+++ Player.java
@@ -1,3 +1,6 @@
+import java.util.Stack;
+import java.util.ArrayList;
+
 /**
  * 
  * 
@@ -7,5 +10,47 @@
  */
 public class Player 
 {
+    private String name;
+    private boolean isTurn;
+    private Stack<Card> numberCardHand;
+    private ArrayList<Card> trumpCardHand;
+
+    /**
+     * [write description here]
+     */
+    public Player()
+    {
+
+    }
+
+    /**
+     * [write description here]
+     * @param name
+     */
+    public Player(String name)
+    {
+
+    }
+
+    /**
+     * [write description here]
+     * @param port
+     * @return
+     */
+    public boolean createRoom(int port)
+    {
+        return false; //TODO fix this
+    }
+
+    /**
+     * [write description here]
+     * @param port
+     * @return
+     */
+    public boolean joinRoom(int port)
+    {
+        return false; //TODO fix this
+    }
+
     
 }
