5e0d6a65a597b90f9796d648d3eb505ef19c9ff4
blee114 <leebrian988@gmail.com>
2025-05-13 10:29:03 -0700

    Update NumberCard.java

--- NumberCard.java
+++ NumberCard.java
@@ -5,7 +5,27 @@
  * @version
  * 
  */
-public class NumberCard 
+public class NumberCard implements Card
 {
-    
+    // attributes
+
+    private int value;
+    private boolean isHidden;
+
+
+    // constructor
+    public NumberCard() {
+        value = -1;
+        isHidden = false;
+    }
+
+
+    // methods
+    public int getValue() {
+        return value;
+    }
+
+    public boolean isHidden() {   
+        return isHidden;
+    }
 }
