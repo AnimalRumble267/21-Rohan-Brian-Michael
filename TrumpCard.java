69ff138e1540665cc8cedadfb840ff5081e358ce
blee114 <leebrian988@gmail.com>
2025-05-13 10:43:09 -0700

    Update TrumpCard.java

--- TrumpCard.java
+++ TrumpCard.java
@@ -14,16 +14,38 @@ public class TrumpCard implements Card
     // construcotrs
 
     public TrumpCard() {
-
-
+        value = 0;
+        type = "";
+        isPlayed = false;
     }
 
+    
+    public TrumpCard(int value, String type) {
+        this.type = type;
+        this.value = value;
+        this.isPlayed = false;
+    }
 
     public TrumpCard(String t, 
 
     // methods
 
+    public int getValue() {
+        return value;
+    }
 
+    public String getType() {
+        return type;
+    }
+
+    public boolean isPlayed() {
+        return isPlayed;
+    }
+    
+    public void setIsPlay(boolean status) {
+        isPlayed = status;
+    }
+    
 
 
     // gui
