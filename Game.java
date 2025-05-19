34a88d79c7fbe0008b9642476964caeab40b7692
blee114 <leebrian988@gmail.com>
2025-05-19 09:58:57 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -11,7 +11,19 @@ public class Game
     public static void main(String[] args)
     {
         /*
-         * Instantiate Players
+         * [ignore] intializes all other class (major classes, which involves instantiates Dealer, Deck, Player, Card, NumberCard, TrumpCard, and GUI classes,  
+         * [ignore] which involve GameGUI and PlayerGUI
+         * 
+         * game loop (input name) -- use scanner; wait for both of the players to submit their name;
+         * constantly check for player turn, and whether or not player is alive
+         * 
+         * deck, name, loop rounds until deck returns exit code of either player winning/losing (game deals with punish)
+         * player pannel is visible based on if it is their turn
+         * restart by asking the names
+         * 
          */
+
+        
+        
     }
 }
