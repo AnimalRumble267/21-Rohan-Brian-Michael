1d8fbd7eb714edb3015c88876553d2b9ab52539c
blee114 <leebrian988@gmail.com>
2025-05-20 04:31:48 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -84,83 +84,95 @@ public class Game
             // first turn in one round
 
             // 1 means player 1 went first, 2 means player 2 went first
-            int temp;
+            int turnIndicator;
+            
+            // 1 = end game, someone died; 2 = passed the turn; 3 = drew a trump card, ask for another action
+            int returnerAction = -1;
             
             if (player1.isTurn) {
                 playerAction(player1, 1, dealer.getBet());
-                temp = 1;
+                
+                turnIndicator = 1;
             }
             else {
                 playerAction(player2, 2, dealer.getBet());
-                temp = 2;
+                turnIndicator = 2;
             }
-            
+
+            // check for punishment
+            if (returnerAction == 1) {
+                resetGame();
+            }
+                
+            else if (returnerAction == 3) {
+                if (turnInicator == 1) {
+                    playerAction(player1, 1, dealer.getBet());
+                }
+                else { 
+                    playerAction(player2, 2, dealer.getBet());
+                }
+            }
+
             //second turn in one round
-            if (temp == 1) {
+            if (turnIndicator == 1) {
                 playerAction(player2, 2, dealer.getBet());
             }
+                
             else {
                 playerAction(player1, 1, dealer.getBet());
             }
-            
-            // TODO scanner action for mouseclick
 
             // check for punishment
-
-            if (player1.numberCardHand
+            if (returnerAction == 1) {
+                resetGame();
+            }
                 
+            else if (returnerAction == 3) {
+                if (turnInicator == 1) {
+                    playerAction(player2, 2, dealer.getBet());
+                }
+                else { 
+                    playerAction(player1, 1, dealer.getBet());
+                }
+            }
             
         }
-
-        // reset game will be handled in playerAction 
     }
 
-    public static int playerAction(Player player, int playerNumber, int action) 
-    {
+    public static void playerAction(Player player, int playerNumber, int action) {
         // note: selecting trump card does not end your turn
-
-        // 1 = end game, someone died; 2 = passed the turn; 3 = drew a trump card, ask for another action
-        int temp = -1;
         
-        
-        if (action == 1) 
-        {
+        if (action == 1) {
             // hit: draw, calcaulte value, if it's over 21 run punishment
             player.giveNumberCard(active); // TODO how to handle trump card...
             player.update();
-            if (player.calcualte() > 21) 
-            {
+            if (player.calcualte() > 21) {
                 boolean deathStatus = dealer.punish(player, dealer.getBet());
-                if (deathStatus) 
-                {
-                    return 1;
+                if (deathStatus) {
+                    returnerAction =  1;
                 }
             }
         }
 
-        else if (action == 2) 
-        {
+        else if (action == 2) {
             // stand: end player turn
             player.setTurn(false);
 
-            if (playerNumber == 1)
-            {
+            if (playerNumber == 1) {
                 player2.setTurn(true);
             }
                 
-            else 
-            {
+            else {
                 player1.setTurn(true);
             }
 
-            return 2;
+            returnerAction = 2;
         }
 
-        else 
-        {
+        else {
+            // TODO: activate the action for the trump card
             int trumpCardIdentifer = action - 3; 
-            // TODO do action based on trump card
-            return 3;
+            returnerAction = 3;
         }
     }
 
